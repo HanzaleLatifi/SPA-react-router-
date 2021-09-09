2684f1fd5e88e68f8b9000c22538258413724b25
h_latifi8 <h.latifi03@gmail.com>
2021-09-09 12:08:50 +0430

    create Navigator component & Link,withRouter(hoc)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import {BrowserRouter , Route} from 'react-router-dom' ;
+import Navigation from './components/Navigation';
 import AboutUsPage from './pages/AboutUsPage';
 import HomePage from './pages/HomePage';
 function App() {
   return (
     <BrowserRouter>
+      <Navigation/>
       <Route path="/" exact={true} component={HomePage} HOME  />
       <Route path="/about-us" component={AboutUsPage} AboutUS />
     </BrowserRouter>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import  {link} from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
 function Navigation() {
     return (
         <header>
             <nav>
                 <li>
-
+                    <Link to='/'>Home</Link>
                 </li>
                 <li>
-
+                    <Link to='/about-us'>AboutUs</Link>
                 </li>
             </nav>
         </header>
     )
 }
 
-export default Navigation
+export default withRouter(Navigation)
